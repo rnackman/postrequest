6b66e387c1108a179497f1895b0c1883cdaa8fb4
rnackman <rachel.nackman@gmail.com>
2015-03-16 18:36:27 -0400

    Change sqlite3 to pg.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def login_or_logout
     if logged_in?
-      "Hello, #{current_user.name}! #{link_to "Log Out", logout_path}".html_safe
+      "<li><a>Hello, #{current_user.name}!</a></li> <li>#{link_to "Log Out", logout_path}</li>".html_safe
     else
       "#{link_to "Sign Up", signup_path} #{link_to "Log In", login_path} Log in to edit, delete, or claim your topics!".html_safe
     end
